d25749cb37ef1725654f4b71aa4049b08924ffa9
Azat Sharip <60136981+AzatSharip@users.noreply.github.com>
2020-05-14 20:02:46 +0500

    creared but runner fo script

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
 import smtplib  # Импортируем библиотеку по работе с SMTP
@@ -9,14 +9,16 @@ import time
 
 
 def price_checker(url):
-    time.sleep(300)
+    ''' Парсим сайт yahoo.finance с задержкой и берем стоимость акции в реальном времени'''
+    time.sleep(1)
     r = requests.get(url)
     #print(r.status_code)
     soup = BeautifulSoup(r.text, features="html.parser")
-    price = soup.find_all("div", {"class": "My(6px) Pos(r) smartphone_Mt(6px)"})[0].find_all(text=True, recursive=True)
-    price = float(price[0].replace(',', ''))
+    pars_price = soup.find_all("div", {"class": "My(6px) Pos(r) smartphone_Mt(6px)"})[0].find_all(text=True, recursive=True)
+    price = float(pars_price[0].replace(',', ''))
+    market_status = pars_price[2]
     print(price)
-    return price
+    return price, market_status
 
 def email_sender(price):
     addr_from = "matvei.elagin87@gmail.com"  # Адресат
@@ -42,7 +44,9 @@ if __name__ == '__main__':
     url = 'https://finance.yahoo.com/quote/LKOH.ME?p=LKOH.ME&.tsrc=fin-srch'
     status = True
     while status:
-        price = price_checker(url)
+        price, market_status = price_checker(url)
+        print(market_status)
+
         if price >= 5200:
             email_sender(price)
             status = False
